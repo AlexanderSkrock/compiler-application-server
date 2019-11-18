7da2569db436996fde93dcf15763a7eadbbbb452
Alexander Skrock <alexanderskrock@gmx.de>
2019-11-18 23:13:55 +0100

    initial spring project setup

---
+++ src/main/java/de/alsk/compilerapplicationserver/CompilerApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package de.alsk.compilerapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilerApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompilerApplicationServerApplication.class, args);
+    }
+
+}
